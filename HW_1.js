c4dfd052bfe397d6567739b1d73d291c576b0384
YongZhouLAGCC <yongzhou168@gmail.com>
2017-11-17 17:28:59 -0500

    updat

--- HW_1.js
+++ HW_1.js
@@ -1,86 +1,44 @@
+// // Homework II
+// 1. factorial
+
+var j=5; 
+var z=1; 
+
+do{
+    z =z*j;
+    j--;
+    
+} while(j>=2)
+console.log(z)
 
 
-var range = 1000; 
+console.log("HW2: Palindrome ~~~~~~~~~~~~~~~~~~~~~~```")
 
-function evenNum() {
-    for(var i =1; i<=range; i++) {
-        if(i%2==0){
-            console.log(i);
+
+function palindrome(input) {
+    var checkPaLin = input;
+    var leng = checkPaLin.length; 
+    var firstHalf = leng/2; 
+    var lastHalf=leng/2;
+    
+     console.log('leng: ',leng)
+
+     console.log('first half length: ',firstHalf)
+    for(var i=0; i< firstHalf; i++)
+    {
+
+        // console.log(checkPaLin.charAt(i));
+        // console.log("~~~~~~~",leng-1);
+        // console.log(checkPaLin.charAt(leng-1));
+
+        if( checkPaLin.charAt(i) ==  checkPaLin.charAt(leng-1-i)) {
+            continue; 
+        } else {
+            console.log("It is not Palindrome")
+            return "-1";
+          
         }
     }
-}
+    return checkPaLin;
 
-console.log('Second: three faces or  five buses . ~~~~~~~~~~~~~~~~~~~~~~`\n')
-
-var faceBus =100;
-
-
-
-
-(function FOrB() {
-    for(var i =3; i< faceBus; i++) {
-        if(((i%3 ==0) || (i%5 ==0))) {
-
-            if(i%3==0 && i%5 !=0){
-                console.log(i, ": ", 'Fizz');
-                
-            } else if(i%5==0 && i%3 !=0) {
-                console.log(i, ": ","Buzz");
-            }   else if(((i%3 ==0) && (i%5 ==0))) {
-                console.log(i, ": ","Fizz" ," ","Buzz")
-               
-            }
-            
-}} 
-})()
-
-
-
-
-
-console.log('Third: button shift, tags will show on or off ~~~~~~~~~~~~~~~~~~~~~~`\n')
-
-var tryButton = document.getElementById("button");
-
-tryButton.addEventListener("click",show);
-
-function show(click) {
-    if(document.getElementById('sampleshowtext').innerHTML=="")
-    {
-        var textShow = document.getElementById("button").innerHTML;
-        document.getElementById('sampleshowtext').innerHTML= textShow;
-        document.getElementById("button").innerHTML= "Text Showed!"
-    } else {
-        document.getElementById('sampleshowtext').innerHTML="";
-    }
-}
-
-
-
-
-
-
-console.log("Fifth: tag showing will follow input above")
-
-var inputType = document.getElementById("Yourinput");
-var follow = document.getElementById("followInput"); 
-
-inputType.addEventListener("input", add);
-
-function add() {
-    var typing = inputType.value;
-    follow.innerHTML = typing;
-}
-
-
-console.log("Practice for tpying and follow in another box")
-
-var writers = document.getElementById("writer");
-var anotherWriters = document.getElementById("String");
-
-writers.addEventListener("input", act);
-
-function act() {
-    var recall = writers.value; 
-    anotherWriters.value=recall;
-}
+} console.log(palindrome('bab'))
